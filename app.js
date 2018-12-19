6af297f28f8e6385fe73f67fddd47b8ccb30d545
villeatuomi <44373490+villeatuomi@users.noreply.github.com>
2018-12-19 15:44:47 +0200

    Versio 2
    Sisältää lisää työvuorovaihtoehtoja, esimerkiksi vapaapäivä, lauantai ja custom-aika. Valitsemalla vapaapäivän, ohjelma ei lisää kyseiselle päivälle tapahtumaa. Kommentteja ja muutujien nimiä on muutettu.

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 
 //------------------------------------SELEKTORIT--------------------------------------------------------------------------
 // valitse ensimmäinen formi id:n perusteella ja tee siitä muuttuja frmdata
-const frmdata = document.querySelector('#formEssi');
+const frmdata = document.querySelector('#form1');
 // valitse toinen lomake id:n perusteella ja tee siitä muuttuja frm2data
 const frm2data = document.querySelector('#form2');
-// valitse toinen lomakkeen h6-elementti ja tallenna se muuttujaksi frm2h6
+// valitse toinen lomakkeen h6-elementti ja tallenna se muuttujaksi frm2h6. Tähän muutetaan vuoronsyötössä kyseinen päivämäärä
 const frm2h6 = document.querySelector('#form2 h6');
 //------------------------------------------------------------------------------------------------------------------------
 
@@ -17,11 +17,11 @@ document.querySelector('#div3').style.display = 'none';
 
 //------------------------------------FUNKTIOT----------------------------------------------------------------------------
 // tee funktio 'teeCSV', jota käytetään skriptin lopussa tapahtumankäsittelijässä
-const teeCSV = (evtEssi) => {
+const teeCSV = (evt1) => {
 
-  // estä klikatessa formin submit
-  evtEssi.preventDefault();
-
+  // estä klikatessa form1:n submit
+  evt1.preventDefault();
+  //-------------------------------
 
   // luo muuttujat, joihin poimitaan lomakkeesta käyttäjän syöttämät tiedot
   const tapahtumaNimi = frmdata.querySelector('input[name="nimi"]').value;
@@ -45,46 +45,48 @@ const teeCSV = (evtEssi) => {
   const paivatLkm = momentAikaero + 1;
   //console.log('Kuinka monta eri päivämäärää mahtuu annettujen päivämäärien väliin (mukaanlukien ensimmäinen ja viimeinen päivä): ' + paivatLkm);
 
-  // muuta toisen lomakkeen h6 vastaamaan käyttäjän syöttämää ensimmäistä päivää
-  frm2h6.innerHTML = ap.format("dddd, MMMM D YYYY");
-
-
+  // näytä div2 ja piilota div1
   document.querySelector('#div2').style.display = 'block';
   document.querySelector('#div1').style.display = 'none';
 
+  // luo string-muuttuja 'tulostettava', joka lopuksi printataan sivulle ja sisältää CSV-muotoisen tekstin
   let tulostettava = '';
+  // lisää stringin perään string, jossa on tarvittava otsikkorivi rivinvaihdon kanssa
   tulostettava += 'Subject,Start date,Start time,End date,End time<br>';
-  const pvmJostaAlkaa = frmdata.querySelector('input[name="alkaapvm"]').value;
 
-  const pvmJohonLoppuu = frmdata.querySelector('input[name="loppuupvm"]').value;
-
-  let juoksevaPaivaTEST = moment('' + pvmJostaAlkaa + '');
-  let endDay = moment('' + pvmJohonLoppuu + '');
-  let loppu = endDay.format('L');
-  //console.log(juoksevaPaivaTEST + ' <- Tämä on muuttuja juoksevaPaivaTEST');
+  // !!! KIRJOITA TÄHÄN SOPIVA KOMMENTTI !!!
+  let loppu = lp.format('L');
 
 
+  // luo muuttuja 'juokseva', johon XXX...
+  let juokseva = ap;
+  // muuta toisen lomakkeen h6 vastaamaan käyttäjän syöttämää 'ensimmäistä päivää' juuri ennen kuin käyttäjä alkaa syöttämään ensimmäisen päivä työvuoroa
+  frm2h6.innerHTML = juokseva.format("dddd, MMMM D YYYY");
+  //
+  console.log('Syötä nyt päivämäärän ' + juokseva.format("dddd, MMMM D YYYY") + ' työvuoro ja paina Lähetä!');
+
+
+
+
+  //------------VUOROJEN KYSYMINEN----------------------------------------------------------
 
   // tee funktio 'kysyVuorot', jota käytetään skriptin lopussa tapahtumankäsittelijässä
   const kysyVuorot = (evtVuorot) => {
 
-    // estä klikatessa formin2 submit
+    // estä klikatessa form2:n submit
     evtVuorot.preventDefault();
     //-------------------------------
 
+    // !!! KIRJOITA TÄHÄN SOPIVA KOMMENTTI !!!
+    let juokseva2 = juokseva.format('L');
 
-    let p = juoksevaPaivaTEST.format('L');
-    console.log(p);
 
 
-    tulostettava += tapahtumaNimi;
-    tulostettava += ',';
-    tulostettava += juoksevaPaivaTEST.format('L');
-    tulostettava += ',';
 
 
     // luo muuttuja, johon tallennetaan Nodelista kaikista vuoroista
     let valittuVuoro = frm2data.querySelectorAll('input[name="vuoro"]');
+    // käy Nodelist läpi ja tallenna muuttujan 'valittuVuoro' arvoksi se, joka on valittu
     valittuVuoro.forEach((a) => {
       //console.log(a.checked);
       if (a.checked) {
@@ -92,148 +94,103 @@ const teeCSV = (evtEssi) => {
         //console.log(valittuVuoro + 'moikka vaan');
       }
     });
-    // TESTI konsoli loggaa valittuVuoro
-    console.log(valittuVuoro + ' <- toi oli käyttäjän syöttämä eka vuoro?');
 
-    let jaettu = valittuVuoro.split('-');
+    // käyttäjän valitessa custom-vuoron
+    if (valittuVuoro == 'CUSTOM' && frm2data.querySelector('input[name="customStart"]').value != '' && frm2data.querySelector('input[name="customEnd"]').value != '') {
+      
+      let customAlkamisaika = frm2data.querySelector('input[name="customStart"]').value;
+      console.log('CustomAlkamisaika:n arvo on: ' + customAlkamisaika);
+      
+      let customLoppumisaika = frm2data.querySelector('input[name="customEnd"]').value;
+      console.log('CustomLoppumisaika:n arvo on: ' + customLoppumisaika);
+      
+      // luodaan apumuuttujat stringien muokkausta varten
+      let part1, part2;
 
-    tulostettava += jaettu[0];
-    tulostettava += ',';
-    tulostettava += juoksevaPaivaTEST.format('L');
-    tulostettava += ',';
-    tulostettava += jaettu[1];
-    tulostettava += ',<br>';
+      // ajan ollessa iltapäivä, niin ei poisteta nollaa '0' tuntien edestä. Lisätään PM
+      if (customAlkamisaika[0] != '0') {
+        part1 = customAlkamisaika.concat(' ', 'PM');
+      }
 
-    console.log(tulostettava);
+      if (customLoppumisaika[0] != '0') {
+        part2 = customLoppumisaika.concat(' ', 'PM');
+      }
 
-    console.log(p + ' Tämä on p:n arvo');
-    console.log(loppu + ' Tämä on loppu:n arvo');
+      // ajan ollessa aamupäivä, niin poistetaan nolla '0' tuntien edestä. Lisätään AM
+      if (customAlkamisaika[0] == '0') {
+        part1 = customAlkamisaika.substr(0);
+        part1 = customAlkamisaika.concat(' ', 'AM');
+      }
 
-    if (p == loppu) {
+      if (customLoppumisaika[0] == '0') {
+        part2 = customLoppumisaika.substr(0);
+        part2 = customLoppumisaika.concat(' ', 'AM');
+      }
 
+      let customValue = part1.concat('-', part2);
+      console.log(customValue);
+      
+      // tallennetaan 'valittuVuoro'-muutujan 'CUSTOM'-arvon päälle customaika muuttujana 'customValue'
+      valittuVuoro = customValue;
+    }
+
+
+
+    // konsoli loggaa valittuVuoro
+    console.log('Syötit vuoron: ' + valittuVuoro + ' päivämäärälle: ' + juokseva.format("dddd, MMMM D YYYY") + ' KIITOS!');
+
+    if (valittuVuoro != 'vapaa') {
+
+      // lisää stringin jatkoksi tapahtuman tiedot
+      tulostettava += tapahtumaNimi;
+      tulostettava += ',';
+      tulostettava += juokseva2;
+      tulostettava += ',';
+
+
+      // splittaa radiobuttonin value arrayksi käyttämällä separaattorina '-'
+      let jaettu = valittuVuoro.split('-');
+
+      tulostettava += jaettu[0];
+      tulostettava += ',';
+      tulostettava += juokseva2;
+      tulostettava += ',';
+      tulostettava += jaettu[1];
+      tulostettava += ',<br>';
+
+      console.log('Tässä on tähän asti kertynyt CSV-string: ' + tulostettava);
+
+      console.log(juokseva2 + ' Tämä on juokseva2:n arvo, joka vastaa siitä, mikä on viimeisin päivä, joka on voitu lisätä CSV-stringiin');
+      console.log(loppu + ' Tämä on loppu:n arvo, joka vastaa siitä, mikä on viimeinen päivä, joka tullaan lisäämään CSV-stringiin. Tämän jälkeen tulostetaan valmis string.');
+
+    } 
+
+
+    if (juokseva2 == loppu) {
       document.querySelector('#div3').style.display = 'block';
       document.querySelector('#div3 p').innerHTML = tulostettava;
       document.querySelector('#div2').style.display = 'none';
-
       return;
-
     }
 
-    //console.log(juoksevaPaivaTEST);
-    juoksevaPaivaTEST = juoksevaPaivaTEST.add(1, 'day');
-    //console.log(juoksevaPaivaTEST.format());
 
-
-    //vuorot[p] = valittuVuoro;
-    //console.log(vuorot + 'moi moi moi');
-    // tais toimia
-
-
+    // kasvata muuttujaa 'juokseva' yhdellä päivällä
+    juokseva = juokseva.add(1, 'days');
+    // näytä 'Lähetä' klikkauksen jälkeen seuraavan päivä päivämäärä div2:n h6:ssa
+    frm2h6.innerHTML = juokseva.format("dddd, MMMM D YYYY");
+    console.log('Syötä nyt päivämäärän ' + juokseva.format("dddd, MMMM D YYYY") + ' työvuoro ja paina Lähetä!');
 
   };
 
 
+  //------------------------------------TAPAHTUMANKÄSITTELIJÄT--------------------------------------------------------------
+  // tapahtumankäsittelijä, joka kuuntelee toisen lomakkeen submit-käskyä ja tällöin suorittaa funktion 'kysyVuorot'
   frm2data.addEventListener('submit', kysyVuorot);
+  //------------------------------------------------------------------------------------------------------------------------  
 };
-
-
-
-// tehdään olio, mihin laitetaan käyttäjän syöttämät vuorot ja niitä vastaavat päivämäärät
-let vuorot = {};
-console.log(vuorot);
 
 
 //------------------------------------TAPAHTUMANKÄSITTELIJÄT--------------------------------------------------------------
 // tapahtumankäsittelijä, joka kuuntelee ensimmäisen lomakkeen submit-käskyä ja tällöin suorittaa funktion 'teeCSV'
 frmdata.addEventListener('submit', teeCSV);
-// tapahtumankäsittelijä, joka kuuntelee toisen lomakkeen submit-käskyä ja tällöin suorittaa funktion 'kysyVuorot'
-//frm2data.addEventListener('submit', kysyVuorot);
 //------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//-----------------------Kommentteja-------------------------------------------
-
-
-
-// Mitä tietoja tarvitaan käyttäjältä: tapahtuman nimi, työvuorojen alkamispvm, työvuorojen päättymispvm
-
-
-//-----------------Villen kokeilu alkaa---------------------------------------
-
-/*
-// poimitaan formista alkamisajat ja päättymisajat
- const tapahtumaNimi = frmdata.querySelector('input[name="nimi"]').value;
- const alkaapvm = frmdata.querySelector('input[name="alkaapvm"]').value;
- const loppuupvm = frmdata.querySelector('input[name="loppuupvm"]').value;
-
-// splitataan ne arrayksi
- const ap = alkaapvm.split('-');
- const ak = alkaaklo.split(':');
- const lp = loppuupvm.split('-');
- const lk = loppuuklo.split(':');
-
-// luodaan uudet aika-objektit (Date-object), johon laitetaan arvoiksi edellisen indeksejä
- const d1 = new Date(ap[0],ap[1],ap[2],ak[0],ak[1]);
- const d2 = new Date(lp[0],lp[1],lp[2],lk[0],lk[1]);
-
- if (d1 >= d2) {
-   alert('INVALID DATES!!!');
- } else {
-   alert('yay!');
- }
-
- console.log(ap);
- console.log(ak);
- console.log(lp);
- console.log(lk);
-
- console.log(d1);
- console.log(d2);
-*/
-
-//-----------------Villen kokeilu loppuu---------------------------------------
